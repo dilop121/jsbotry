0a9ee229816b904d930e81352bbbff14d218516e
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 01:49:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const nandha = new Telegraf(config.BOT_TOKEN);
 
 
 nandha.command("start", async (message) => {
-        const name = message.from.first_name
+        var mention = "[${message.from.first_name}](tg://openmessage?id=${message.from.id})";
 	try { await message.replyWithAnimation(
             { url: "https://graph.org/file/14b56501d51e1ad420a9d.mp4" },
             { caption: config.START_TEXT }
