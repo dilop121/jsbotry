a19a0c14d65cca6cae456b02b767a7ece9005dbb
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-30 21:13:35 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  BOT_TOKEN: '6077850923:6081726722:AAGIElY8tVYC57YHhEufT11I2n5oCacn6Qo', // your telegram bot token, get from @botfather
+  SUPPORT_CHAT: -1001717881477,       // your bot support chat group chat id
+}
