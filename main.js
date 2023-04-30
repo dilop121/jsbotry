961370df1249ffd738adee763f1ebb29b481ae8a
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 01:10:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,17 +14,23 @@ const config = require('./config');
 const nandha = new Telegraf(config.BOT_TOKEN);
 
 
-
-nandha.start((message) => {
-  message.reply('Hello ' + message.from.first_name + '!');
+nandha.command("start", async (message) => {
+        const name = message.from.first_name
+	await message.replyWithAnimation(
+            { url: "https://graph.org/file/14b56501d51e1ad420a9d.mp4" },
+            { caption: config.START_TEXT }
+        );
 });
 
 
-nandha.help((message) => {
+nandha.help(async (message) => {
     const name = message.from.first_name
-    message.reply(
-      `Hello ${name} We are continuously updating this Bot to add more futures in the near future.`);
-})
+    await message.reply(
+      `Hello ${name} We are continuously updating this Bot to add more futures in the near future.`
+    );
+});
+
+
 
 
 nandha.launch();
