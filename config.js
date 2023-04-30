e7771cb8a4ac3c7efd0d70d5c61c465f2ba3036f
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 01:35:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,5 @@ module.exports = {
   SUPPORT_CHAT: -1001717881477,       // your bot support chat group chat id
   START_TEXT: `Hello there! I am Gabimaru,
 your friendly anime-themed bot.
-As a boy gendered bot powered by node.js,
-I am ready to assist and entertain you. So,
-what can I help you with today? Let's get started!`
+As a boy gendered bot powered by node.js, I am ready to assist and entertain you. So, what can I help you with today? Let's get started!`
 }
