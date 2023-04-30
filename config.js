af5c0c3b2b809ffda9695d2458a3cc98a1bc13ce
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 01:10:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,9 @@
 module.exports = {
   BOT_TOKEN: '6077850923:AAFGCn1D8pB3bZla186CF9VlurbaA9TsSgI', // your telegram bot token, get from @botfather
   SUPPORT_CHAT: -1001717881477,       // your bot support chat group chat id
+  START_TEXT: `Hello there! I am Gabimaru,
+your friendly anime-themed bot.
+As a boy gendered bot powered by node.js,
+I am ready to assist and entertain you. So,
+what can I help you with today? Let's get started!`
 }
