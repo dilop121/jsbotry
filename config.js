94f4512189a534076579329dd572ff0458dce4d6
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-30 22:35:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  BOT_TOKEN: '6077850923:6081726722:AAGIElY8tVYC57YHhEufT11I2n5oCacn6Qo', // your telegram bot token, get from @botfather
+  BOT_TOKEN: '6077850923:AAFGCn1D8pB3bZla186CF9VlurbaA9TsSgI', // your telegram bot token, get from @botfather
   SUPPORT_CHAT: -1001717881477,       // your bot support chat group chat id
 }
