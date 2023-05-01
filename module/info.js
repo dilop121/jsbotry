e7a489f06246052e220316b0527c11776e96370a
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-05-01 20:05:59 +0530

    Update info.js

--- module/info.js
+++ module/info.js
@@ -7,7 +7,7 @@ const { Telegraf } = require("telegraf")
 const nandha = new Telegraf(config.BOT_TOKEN);
 
 
-nandha.ok(async (ctx) => {   
+nandha.on(async (ctx) => {   
      const message_id = ctx.message.message_id;
      await ctx.reply(
           {text: 'yes?', reply_to_message_id: message_id}
