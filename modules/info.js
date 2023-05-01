ab30645e5c3a4fb5b2c83253de59057c9ba7f6a7
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 19:06:02 +0530

    Update info.js

--- modules/info.js
+++ modules/info.js
@@ -1,1 +1,14 @@
 // test
+
+
+
+const { nandha } = require('./main');
+
+
+nandha.ok(async (ctx) => {   
+     const message_id = ctx.message.message_id;
+     await ctx.reply(
+          {text: 'yes?', reply_to_message_id: message_id}
+);
+});
+    
