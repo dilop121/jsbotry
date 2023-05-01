2c5254b4bb329dda97d74afcbfae86e771270c7c
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-05-01 22:09:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,39 @@
-
-
-// This Repository made by @NandhaBots
-// kindly asking support us
-
-
-
+//This repo is owned by t.me/Nandhabots if u ever copy or use it, kindly give us credits and Thanks for using this repo 
 
 
 const config = require('./config');
-
-
-const { Telegraf } = require("telegraf")
+const { Telegraf } = require("telegraf");
 const nandha = new Telegraf(config.BOT_TOKEN);
 
+const { infouser, lmao } = require('./module/info.js')(nandha); // pass nandha as parameter
 
-const info = require('./module/info');
-
+infouser();
+lmao("bye")
 
 nandha.command("start", async (ctx) => {
-        const message_id = ctx.message.message_id // alternative can use `const` as `var`
-	try { await ctx.replyWithAnimation(
+    const message_id = ctx.message.message_id;
+    try {
+        await ctx.replyWithAnimation(
             { url: "https://graph.org/file/14b56501d51e1ad420a9d.mp4" },
-            { caption: config.START_TEXT,
-              reply_to_message_id: message_id }
-              );
-        } catch (err) {
-           await ctx.reply(`Error: ${err}`)
-        }
+            { caption: config.START_TEXT, reply_to_message_id: message_id }
+        );
+    } catch (err) {
+        await ctx.reply(`Error: ${err}`)
+    }
 });
 
-
 nandha.help(async (ctx) => {
-    const name = ctx.from.first_name
-    const message_id = ctx.message.message_id
+    const name = ctx.from.first_name;
+    const message_id = ctx.message.message_id;
     await ctx.reply(
-      { text: `Hello ${name} We are continuously updating this Bot to add more futures in the near future.`
-        , reply_to_message_id: message_id }
+      { text: `Hello ${name} We are continuously updating this Bot to add more futures in the near future.`,
+        reply_to_message_id: message_id }
     );
 });
 
-
-
 nandha.launch();
-console.log("=> GABIMARU-BOT has been Launched!")
-
+console.log("=> GABIMARU-BOT has been Launched!");
 
 // Enable graceful stop
 process.once('SIGINT', () => nandha.stop('SIGINT'));
 process.once('SIGTERM', () => nandha.stop('SIGTERM'));
-
-
-
-
