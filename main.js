f9ff6370c25250db35d524ee14af9ad6da36ceec
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 08:36:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,13 +27,12 @@ nandha.command("start", async (message) => {
         }
 });
 
-
 nandha.help(async (message) => {
     const name = message.from.first_name
     const message_id = message.message_id
     await nandha.sendMessage(
-      text: `Hello ${name} We are continuously updating this Bot to add more futures in the near future.`
-      , { reply_to_message_id: message_id }
+      { text: Hello ${name} We are continuously updating this Bot to add more futures in the near future.
+        , reply_to_message_id: message_id }
     );
 });
 
