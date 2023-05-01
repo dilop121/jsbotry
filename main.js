ec1251b7584452df305223842e19a03b390e5f54
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 08:01:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 // This Repository made by @NandhaBots
-// were kindly asking please support us
+// kindly asking support us
 
 
 
@@ -15,10 +15,12 @@ const nandha = new Telegraf(config.BOT_TOKEN);
 
 
 nandha.command("start", async (message) => {
-        var mention = "[${message.from.first_name}](tg://openmessage?id=${message.from.id})";
+        const mention = "[${message.from.first_name}](tg://openmessage?id=${message.from.id})";
+        const message_id = message.message_id // alternative can use `const` as `var`
 	try { await message.replyWithAnimation(
             { url: "https://graph.org/file/14b56501d51e1ad420a9d.mp4" },
-            { caption: config.START_TEXT }
+            { caption: config.START_TEXT,
+              reply_to_message_id = message_id }
               );
         } catch (err) {
            await message.reply(`Error: ${err}`)
@@ -28,8 +30,10 @@ nandha.command("start", async (message) => {
 
 nandha.help(async (message) => {
     const name = message.from.first_name
+    const message_id = message.message_id
     await message.reply(
-      `Hello ${name} We are continuously updating this Bot to add more futures in the near future.`
+      "Hello ${name} We are continuously updating this Bot to add more futures in the near future."
+      , { reply_to_message_id: message_id }
     );
 });
 
