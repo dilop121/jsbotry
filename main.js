652f770dd06599673da755330ed1040cce166af0
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-01 11:06:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,24 +14,24 @@ const config = require('./config');
 const nandha = new Telegraf(config.BOT_TOKEN);
 
 
-nandha.command("start", async (message) => {
-        const mention = `[${message.from.first_name}](tg://openmessage?id=${message.from.id})`;
-        const message_id = message.message_id // alternative can use `const` as `var`
-	try { await message.replyWithAnimation(
+nandha.command("start", async (ctx) => {
+        const mention = `[${ctx.from.first_name}](tg://openmessage?id=${message.from.id})`;
+        const message_id = ctx.message.message_id // alternative can use `const` as `var`
+	try { await ctx.replyWithAnimation(
             { url: "https://graph.org/file/14b56501d51e1ad420a9d.mp4" },
             { caption: config.START_TEXT,
               reply_to_message_id: message_id }
               );
         } catch (err) {
-           await message.reply(`Error: ${err}`)
+           await ctx.reply(`Error: ${err}`)
         }
 });
 
 
-nandha.help(async (message) => {
-    const name = message.from.first_name
-    const message_id = message.message_id
-    await message.reply(
+nandha.help(async (ctx) => {
+    const name = ctx.from.first_name
+    const message_id = ctx.message.message_id
+    await ctx.reply(
       { text: `Hello ${name} We are continuously updating this Bot to add more futures in the near future.`
         , reply_to_message_id: message_id }
     );
