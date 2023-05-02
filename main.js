77d1b93d21a9e619ad7455f26579e06683abc3f8
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-02 17:44:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,11 +14,19 @@ lmao("bye")
 
 
 nandha.command("json", async (ctx) => {
+
+      let user_id = ctx.message.from.id
+
+      if ( user_id !== 5696053228) {
+           return await ctx.reply("You don't have access for do this!");
+      }
+
       let text = JSON.stringify(ctx);
       fs.writeFileSync("json.txt", text);
       try {
            return await ctx.replyWithDocument(
-        { source: "json.txt", filename: "json.txt" } );
+        { source: "json.txt", filename: "json.txt", 
+           reply_to_message_id: ctx.message.id });
       } catch (error) {
           return await ctx.reply(error.toString());
       }
