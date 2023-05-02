4fedd8f7422b8e8bac18e181c2c778ce10b90078
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-02 15:34:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ nandha.command('logs', (ctx) => {
   // Read the log file
   fs.readFile('example.log', (err, data) => {
     if (err) {
-      console.error(err);
+       ctx.reply(err);
     } else {
       // Send the log file to the chat
       ctx.replyWithDocument({ source: data, filename: 'example.log' });
