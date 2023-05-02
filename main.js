6235d39712359ebd42a107b26aea1ebd93e0c339
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-02 23:24:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,32 +7,12 @@ const nandha = new Telegraf(config.BOT_TOKEN);
 const fs = require('fs');
 
 
-const { infouser, lmao } = require('./module/info.js')(nandha); // pass nandha as parameter
+const { infouser } = require('./module/info.js')(nandha); // pass nandha as parameter
 
 infouser();
-lmao("bye")
 
 
-nandha.command("json", async (ctx) => {
 
-      let user_id = ctx.message.from.id
-
-      if ( user_id !== 5696053228) {
-           return await ctx.reply("You don't have access for do this!");
-      }
-
-      let text = JSON.stringify(ctx);
-      fs.writeFileSync("json.txt", text);
-      try {
-           return await ctx.replyWithDocument(
-        { source: "json.txt", filename: "json.txt"}, 
-           { reply_to_message_id: ctx.message.message_id });
-      } catch (error) {
-          return await ctx.reply(error.toString());
-      }
-      fs.unlinkSync("json.txt");
-
-});
 
 
 
