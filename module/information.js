35cf7e0e5a0638a914769b6b999cb75521dd633d
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-05 08:02:23 +0530

    Update information.js

--- module/information.js
+++ module/information.js
@@ -6,7 +6,7 @@ function information(nandha) {
       nandha.command('info', async (ctx) => {
       let user_id = ctx.message.from.id;
       let message_id = ctx.message.message_id;
-      await nandha.telegram.getUserProfilePhotos(userId).then((photos) => {
+      const await nandha.telegram.getUserProfilePhotos(userId).then((photos) => {
       const fileId = photos.photos[0][photos.photos[0].length - 1].file_id;
       return await ctx.replyWithPhoto({ file_id: fileId });
   
