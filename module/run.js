c5642e6299c9f03e594f6aa74bb7a1b5862ad0dc
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-07 12:09:36 +0530

    Update run.js

--- module/run.js
+++ module/run.js
@@ -4,6 +4,22 @@ const qs = require('qs');
 const axios = require('axios');
 
 
+You can do this in Node.js by defining the list and x variables, and then using the includes() method to check if x is in list:
+
+```
+
+
+if (!list.includes(x)) {
+  return false;
+} else {
+  return true;
+}
+```
+
+This code will return true if "py" is in the list, and false if it is not.
+
+
+
 async function run_code(nandha) {
   nandha.command("run", async (ctx) => {
     var data = qs.stringify({
@@ -11,6 +27,17 @@ async function run_code(nandha) {
       'language': 'py',
       'input': ''
     });
+
+    const list = ["py", "java", "go", "js"];
+    
+    text = ctx.message.text
+
+    let lang = text.split(" ")[1];
+    
+
+    if (!list.includes(lang)) {
+         return await ctx.reply("Enter valid language code!")
+    }
 
     var config = {
       method: 'post',
