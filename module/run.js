f6e39ae8c2f9133b71f3a5f58a83e5793fc914fb
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-07 11:53:24 +0530

    Update run.js

--- module/run.js
+++ module/run.js
@@ -1,20 +1,32 @@
 
 
-
-
+const qs = require('qs');
+const axios = require('axios');
 
 
 function run_code(nandha) {
      nandha.command("run", async (ctx) => {         
-
-          let language = "py";
-          let code = "print('hello')";
-         
-          response = fetch(`https://codex-rest.onrender.com/?language=${language}&code=${code}`) 
-          .then(response => response.json())
-          .then(data => await ctx.reply(data)))
-          .catch(error => await ctx.reply(error.toString())
-          
+      
+     var data = qs.stringify({
+  'code': 'print("hello")',
+  'language': 'py',
+  'input': ''
+});
+     var config = {
+  method: 'post',
+  url: 'https://api.codex.jaagrav.in',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  data: data
+};
+     axios(config)
+  .then(function (response) {
+    await ctx.reply(response.data.output);
+  })
+  .catch(function (error) {
+    await ctx.reply(error.toString());
+  });
 
 });
 
