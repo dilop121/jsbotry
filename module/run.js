34f540e9b87d4245b739b507acea7ddd060d04e6
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-07 11:57:50 +0530

    Update run.js

--- module/run.js
+++ module/run.js
@@ -4,32 +4,30 @@ const qs = require('qs');
 const axios = require('axios');
 
 
-function run_code(nandha) {
-     nandha.command("run", async (ctx) => {         
-      
-     var data = qs.stringify({
-  'code': 'print("hello")',
-  'language': 'py',
-  'input': ''
-});
-     var config = {
-  method: 'post',
-  url: 'https://api.codex.jaagrav.in',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  data: data
-};
-     axios(config)
-  .then(function (response) {
-    await ctx.reply(response.data.output);
-  })
-  .catch(function (error) {
-    await ctx.reply(error.toString());
+async function run_code(nandha) {
+  nandha.command("run", async (ctx) => {
+    var data = qs.stringify({
+      'code': 'print("hello")',
+      'language': 'py',
+      'input': ''
+    });
+
+    var config = {
+      method: 'post',
+      url: 'https://api.codex.jaagrav.in',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: data
+    };
+
+    try {
+      const response = await axios(config);
+      await ctx.reply(response.data.output);
+    } catch (error) {
+      await ctx.reply(error.toString());
+    }
   });
-
-});
-
 }
 
 
