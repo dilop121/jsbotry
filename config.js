ba273f0af81e9ea54852af69a17e6d6d041c132f
Dil <126651350+dilop121@users.noreply.github.com>
2024-01-23 21:46:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  BOT_TOKEN: '6081726722:AAHpKNm4NbFIR9hWdloDZkCA5GC1GEFLH0o', // your telegram bot token, get from @botfather
-  SUPPORT_CHAT: -1001717881477,       // your bot support chat group chat id
+  BOT_TOKEN: '5722401835:AAG-z4kbTbLdWm2xD-lC2utFmb-8CX06G7Y', // your telegram bot token, get from @botfather
+  SUPPORT_CHAT: -1002106283057,       // your bot support chat group chat id
   START_TEXT: `Hello there! I am Gabimaru,
 your friendly anime-themed bot.
 As a boy gendered bot powered by node.js, I am ready to assist and entertain you. So, what can I help you with today? Let's get started!`
